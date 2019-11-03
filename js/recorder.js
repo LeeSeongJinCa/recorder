f3e3b71465c775774924ee412fa4a12fb65d21c6
이성진 <chlskatnr1@naver.com>
2019-11-03 21:04:31 +0900

    changeRealRecorderSound

--- js/recorder.js
+++ js/recorder.js
@@ -1,62 +1,107 @@
 // * soundPlays
 const audio = new Audio();
+const main = document.getElementById("mainHole");
 const lastType = document.getElementById('lastTyping');
 
 function audioPlay(n) {
-    audio.src = `sound/sound${n}.mp3`;
+    audio.src = `./sound/${n}.mp3`;
     audio.oncanplaythrough = () => {
         audio.play();
     }
 }
-function getIndexOfHole(num) {
-    let holeValue = document.querySelectorAll('.holes');
-    return holeValue[num - 1];
-}
+
 function changeHolesColor(tag, sound, changeColor) {
     let parentTag = tag.parentNode;
     if(changeColor) parentTag.classList = sound;
     else            parentTag.classList = "";
 }
+
 function lastTyping(text) {
     lastType.textContent = text;
 }
-function playSound(value, sound) {
-    audioPlay(value);
-    let hole = getIndexOfHole(value);
-    changeHolesColor(hole, sound, true);
-    lastTyping(sound);
-}
-function stopSound(value, sound) {
-    let hole = getIndexOfHole(value);
-    changeHolesColor(hole, sound, false);
-}
+
 document.body.onkeydown = () => {
-    const key = window.event.keyCode;
-    let v;
-    switch (key) {
-        case 65: v = 1; playSound(v, "do"); break;
-        case 83: v = 2; playSound(v, "re"); break;
-        case 68: v = 3; playSound(v, "mi"); break;
-        case 70: v = 4; playSound(v, "fa"); break;
-        case 74: v = 5; playSound(v, "so"); break;
-        case 75: v = 6; playSound(v, "la"); break;
-        case 76: v = 7; playSound(v, "ti"); break;
-        case 186: v = 8; playSound(v, "doo"); break;
+    let s;
+    switch (window.event.keyCode) {
+        case 65:
+            s = "lowDo";
+            audioPlay(s);
+            changeHolesColor(main, s, true);
+            lastTyping(s);
+            break;
+        case 83:
+            s = "re"
+            audioPlay(s);
+            changeHolesColor(main, s, true);
+            lastTyping(s);
+            break;
+        case 68: 
+            s = "mi"
+            audioPlay(s);
+            changeHolesColor(main, s, true);
+            lastTyping(s);
+            break;
+        case 70: 
+            s = "fa"; 
+            audioPlay(s);
+            changeHolesColor(main, s, true);
+            lastTyping(s);
+            break;
+        case 74: 
+            s = "so";
+            audioPlay(s);
+            changeHolesColor(main, s, true);
+            lastTyping(s);
+            break;
+        case 75: 
+            s = "la";
+            audioPlay(s);
+            changeHolesColor(main, s, true);
+            lastTyping(s);
+            break;
+        case 76: 
+            s = "ti";
+            audioPlay(s);
+            changeHolesColor(main, s, true);
+            lastTyping(s);
+            break;
+        case 186: 
+            s = "highDo";
+            audioPlay(s);
+            changeHolesColor(main, s, true);
+            lastTyping(s);
+            break;
         default: return;
     }
 };
+
 document.body.onkeyup = () => {
-    const key = window.event.keyCode;
-    let v;
-    switch (key) {
-        case 65: v = 1; stopSound(v); break;
-        case 83: v = 2; stopSound(v); break;
-        case 68: v = 3; stopSound(v); break;
-        case 70: v = 4; stopSound(v); break;
-        case 74: v = 5; stopSound(v); break;
-        case 75: v = 6; stopSound(v); break;
-        case 76: v = 7; stopSound(v); break;
-        case 186: v = 8; stopSound(v); break;
-        default: return;
+    switch (window.event.keyCode) {
+        case 65:
+            changeHolesColor(main, "", false);
+            break;
+        case 83: 
+            changeHolesColor(main, "", false);
+            break;
+        case 68:
+            changeHolesColor(main, "", false);
+            break;
+        case 70: 
+            changeHolesColor(main, "", false);
+            break;
+        case 74: 
+            changeHolesColor(main, "", false);
+            break;
+        case 75: 
+            changeHolesColor(main, "", false);
+            break;
+        case 76: 
+            changeHolesColor(main, "", false);
+            break;
+        case 186:
+            changeHolesColor(main, "", false);
+            break;
+        default: 
+            return;
     }
 };
